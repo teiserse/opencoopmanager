ef72b5eb995cb0d215649142e4c9848cc1dbc6eb
teiserse <teiserse@tcd.ie>
2020-06-11 19:47:30 +0100

    finished basic one-vote-per-person

--- opencoopmanager/views.py
+++ opencoopmanager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -7,18 +7,23 @@ from .models import Vote, VoteChoice, Ballot
 
 @login_required
 def index(request):
-    vote_list = Vote.objects.order_by('start_date')
+    vote_list = Vote.objects.order_by('start_date'
+            ).filter(remaining_voters=request.user)
     context = {'vote_list': vote_list}
     return render(request, 'opencoopmanager/index.html', context)
 
 @login_required
 def votemain(request, vote_id):
     vote = get_object_or_404(Vote, pk=vote_id)
+    if not request.user in vote.remaining_voters.all():
+        return redirect('opencoopmanager:index')
     return render(request, 'opencoopmanager/votemain.html', {'vote': vote})
 
 @login_required
 def sendballot(request, vote_id):
     vote = get_object_or_404(Vote, pk=vote_id)
+    if not request.user in vote.remaining_voters.all():
+        return redirect('opencoopmanager:index')
     ballot = {}
     for choice in vote.votechoice_set.all():
         try:
@@ -41,4 +46,8 @@ def sendballot(request, vote_id):
             pass
 
     Ballot.create(vote, ballot).save()
-    return HttpResponseRedirect(reverse('opencoopmanager:index'))
+
+    vote.remaining_voters.remove(request.user)
+    vote.save()
+
+    return redirect('opencoopmanager:index')
